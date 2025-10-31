69471b44d8c4785ee1f7e86f0c821d5c3848913c
GavrilaSrs12443 <230712443@students.uajy.ac.id>
2025-11-01 00:19:04 +0700

    migrationCopyBuku

---
+++ database/migrations/2025_10_31_171827_create_copy_buku.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('copy_buku', function (Blueprint $table) {
+            $table->string('id_buku_copy',10)->primary();
+            $table->string('id_buku');
+            $table->string('rak');
+            $table->enum('status', ['dipinjam', 'dikembalikan']);
+            $table->timestamps();
+
+            $table->foreign('id_buku')
+                    ->references('id_buku')
+                    ->on('buku')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('copy_buku');
+    }
+};
