e6283a426967b3ef3b64a605d59588c31a7b4d35
GavrilaSrs12443 <230712443@students.uajy.ac.id>
2025-11-01 00:17:43 +0700

    migrationBukuKategori

---
+++ database/migrations/2025_10_31_171639_create_buku_kategori.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buku_kategori', function (Blueprint $table) {
+            $table->string('id_buku');
+            $table->string('id_kategori');
+            $table->timestamps();
+
+            $table->foreign('id_buku')
+                    ->references('id_buku')
+                    ->on('buku')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+
+            $table->foreign('id_kategori')
+                    ->references('id_kategori')
+                    ->on('kategori')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buku_kategori');
+    }
+};
