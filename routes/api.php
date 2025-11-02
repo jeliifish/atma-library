8ba57aa3a8484016fc4295748c79273cc8d01368
GavrilaSrs12443 <230712443@students.uajy.ac.id>
2025-11-02 18:07:13 +0700

    buat RouteBukuController

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,17 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BukuController;
 
 Route::post('/register', [MemberController::class, 'store']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::get('/buku', [BukuController::class, 'index']);
+Route::post('/buku', [BukuController::class, 'store']);
+Route::get('/buku/{id_buku}', [BukuController::class, 'show']);
+Route::put('/buku/{id_buku}', [BukuController::class, 'update']);
+Route::delete('/buku/{id_buku}', [BukuController::class, 'destroy']);
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
