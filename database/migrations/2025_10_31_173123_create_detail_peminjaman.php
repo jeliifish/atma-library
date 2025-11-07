ff32352e53bd84bd3fbd27dfbb7538c2147a6f7e
Zelika Aninda Prima <zelikaanindap1703@gmail.com>
2025-11-07 17:13:10 +0700

    add status di detailPeminjaman

--- database/migrations/2025_10_31_173123_create_detail_peminjaman.php
+++ database/migrations/2025_10_31_173123_create_detail_peminjaman.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->unsignedBigInteger('nomor_pinjam');
             $table->string('id_buku_copy');
             $table->date('tgl_kembali')->nullable();
+            $table->enum('status', ['dipinjam', 'dikembalikan', 'terlambat']);
             $table->timestamps();
 
             $table->foreign('nomor_pinjam')
