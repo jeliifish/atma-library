374b7ebce33af376fadd5eb3e7f309e79d497b13
GavrilaSrs12443 <230712443@students.uajy.ac.id>
2025-11-09 15:19:28 +0700

    makeModelDetailPembayaranDenda

---
+++ app/Models/DetailPembayaranDenda.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DetailPembayaranDenda extends Model
+{
+    protected $table = 'detail_pembayaran_denda';
+    protected $primaryKey = 'id_detail'; // kalau kamu punya kolom id sendiri
+    public $timestamps = true;
+
+    protected $fillable = [
+        'id_pembayaran',
+        'id_denda',
+        'jumlah_bayar'
+    ];
+
+    public function pembayaranDenda()
+    {
+        return $this->belongsTo(PembayaranDenda::class, 'id_pembayaran', 'id_pembayaran');
+    }
+
+    public function denda()
+    {
+        return $this->belongsTo(Denda::class, 'id_denda', 'id_denda');
+    }
+}
