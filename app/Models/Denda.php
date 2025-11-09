3b29f27ec4155c91fd6573b5f2e8395020413f58
GavrilaSrs12443 <230712443@students.uajy.ac.id>
2025-11-09 15:01:52 +0700

    revModelDenda

--- app/Models/Denda.php
+++ app/Models/Denda.php
@@ -12,14 +12,32 @@ class Denda extends Model
 
     protected $fillable = [
         'nomor_pinjam',
+        'id_buku_copy',
         'hari_telat',
         'harga_per_hari',
-        'total_denda'
+        'total_denda',
+        'status'
     ];
 
     public function peminjaman()
     {
         return $this->belongsTo(Peminjaman::class, 'nomor_pinjam', 'nomor_pinjam');
+    }
+
+    public function copyBuku()
+    {
+        return $this->belongsTo(CopyBuku::class, 'id_buku_copy', 'id_buku_copy');
+    }
+
+    public function pembayaranDenda()
+    {
+        return $this->belongsToMany(
+            PembayaranDenda::class,
+            'detail_pembayaran_denda',
+            'id_denda',
+            'id_pembayaran'
+        )->withPivot('nominal')
+         ->withTimestamps();
     }
 
 }
